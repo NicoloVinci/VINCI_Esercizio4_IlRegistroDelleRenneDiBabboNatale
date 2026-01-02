f7aa1100fb445bc137586d8f2d6427a890d6101f
nicolovinci <12999303@itisrossi.vi.it>
2026-01-02 16:58:14 +0100

    Done the first point of the To-Do List

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <time.h>
 
 typedef struct {
     char nome[30];
@@ -7,6 +12,25 @@ typedef struct {
 } Renna;
 
 int main(void) {
-    printf("Hello, World!\n");
+    srand(time(NULL));
+    int fileDescriptor[2];
+    if (pipe(fileDescriptor) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+    pid_t elf;
+    if ((elf = fork()) == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    if (elf == 0) {
+        int randomNumber = rand() % 8 + 5;
+        for (int i = 0; i < randomNumber; i++) {
+            Renna r;
+        }
+    } else {
+        close(fileDescriptor[1]);
+        wait(NULL);
+    }
     return 0;
 }
