b00785a0b3921eaef689834970546a2804d623ee
nicolovinci <12999303@itisrossi.vi.it>
2026-01-02 16:46:53 +0100

    Created the Struct

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+typedef struct {
+    char nome[30];
+    int velocità;  // km/h
+    int resistenza; // 1-10
+} Renna;
+
 int main(void) {
     printf("Hello, World!\n");
     return 0;
